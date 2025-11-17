e8ead20aeee2b3cd7736a85df5b74ca4c87b4cff
Luan T. Felix <lutavares.bros@gmail.com>
2025-11-16 22:11:23 -0300

    :wrench: adiciona configuração de backend

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,13 @@ export default defineConfig(() => ({
   server: {
     host: "::",
     port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   plugins: [react()],
   resolve: {
